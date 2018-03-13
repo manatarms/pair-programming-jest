3926f927b564710843283f9b4246ac0bcf72c47d
Sachin <sachin@cloudflare.com>
2018-03-13 13:16:32 -0700

    Clarify test case

--- src/utils.test.js
+++ src/utils.test.js
@@ -13,15 +13,15 @@ test.skip("should return null for non array inputs ", () => {
   expect(utils.arrayMerge(2, "a")).toBe(null);
 });
 
-test.skip("should merge arrays", () => {
+test.skip("should merge two arrays", () => {
   expect(utils.arrayMerge([1, 2, 3], [4, 5, 6])).toEqual([1, 2, 3, 4, 5, 6]);
 });
 
-test.skip("should convert object to be [1, 2, 3]", () => {
+test.skip("should convert object values to an array", () => {
   expect(utils.convertObjectToArray({ a: 1, b: 2, c: 3 })).toEqual([1, 2, 3]);
 });
 
-test.skip("should merge objects", () => {
+test.skip("should merge two objects", () => {
   const objectA = { a: 10 }, objectB = { b: 20 };
   expect(utils.mergeObjects(objectA, objectB)).toEqual({ a: 10, b: 20 });
   expect(objectA).toEqual({ a: 10 });
