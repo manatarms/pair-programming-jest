186318db05c4acacc1e2d0811a7fde6bbb4ae4d7
Sachin Fernandes <manatarms@users.noreply.github.com>
2018-05-25 17:51:24 -0700

    Change to lower case s

--- src/utils.test.js
+++ src/utils.test.js
@@ -28,6 +28,6 @@ test.skip("should merge two objects", () => {
   expect(objectB).toEqual({ b: 20 });
 });
 
-test("Should test async code", () => {
+test("should test async code", () => {
   expect(utils.fetchData()).toBe("data");
 });
